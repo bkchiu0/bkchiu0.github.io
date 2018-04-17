1f49ba547e7cad2ba21f0b70e2e95be9ba9aa14f
Brian Chiu <bkchiu0@gmail.com>
2018-04-17 02:34:14 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+M.AutoInit();
+var elems = document.querySelectorAll('.scrollspy');
+for(i=0; i<elems.length; i++){
+	var instance = M.ScrollSpy.init(elems[i]);
+	instance.options.scrollOffset=65;
+}
+var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+});
