8ee49d7fc3d55f450dec5a1a966571f2146cbdfa
Brian Chiu <bkchiu0@gmail.com>
2019-11-09 01:00:52 -0500

    Fix Some Styling

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
+// Init Autoscrolling functionality
 M.AutoInit();
-elems = document.querySelectorAll('.scrollspy');
-for(i=0; i<elems.length; i++){
-	var instance = M.ScrollSpy.init(elems[i]);
-	instance.options.scrollOffset=65;
+elems = document.querySelectorAll(".scrollspy");
+for (i = 0; i < elems.length; i++) {
+  var instance = M.ScrollSpy.init(elems[i]);
+  instance.options.scrollOffset = 65;
 }
 var instance = M.Carousel.init({
-    fullWidth: false,
-    indicators: true
+  fullWidth: false,
+  indicators: true
 });
+
+// Put in auto generated copyright Date
+document.getElementById(
+  "CopyrightDate"
+).innerHTML = `© ${new Date().getFullYear()} Brian Chiu`;
